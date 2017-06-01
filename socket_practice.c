a13b60217501774a786b272de9e34228e27415fb
alapha23 <alapha23@gmail.com>
2017-06-02 03:06:14 +0800

    Modified socket practice

--- socket_practice.c
+++ socket_practice.c
@@ -75,9 +75,9 @@ int main(){
  *set protocol to "0" to have socket() choose the correct protocol based on the type
  *socket() returns a socket descriptor
 */
-	int sock;
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock < 0)
+	int my_sock;
+	my_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (my_sock < 0)
 		perror("Error opening socket");
 
 /* Once done opening a socket, we might have to associate the socket with a port on a local machine
@@ -86,7 +86,7 @@ int main(){
  * 
  * 
 */
-	if( bind(sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) < 0)
+	if( bind(my_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr)) < 0)
 	perror("Error on binding.");
 /*	Some of process of getting your own IP address and port
  *	my_addr.sin_port = 0;	//choose an unused port at random
@@ -94,6 +94,52 @@ int main(){
 0
  * 	All ports below 1024 are RESERVED
 */ 	
+
+/*
+ * int connect (int sockfd, struct sockaddr *serv_addr, int addrlen);
+ * sockfd: socket file descriptor, serv_addr is the struct sockaddr containing destination port and IP address; addrlen is often set to sizeof(struct sockaddr)
+ *
+*/
+	connect(my_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr));
+//NEED to check if connected successfully
+
+/*First we listen(), then we accept()
+ *int listen(int sockfd, int backlog);
+ *backlog is the number of connections allowed on the incoming queue
+ * 
+*/
+	listen(my_sock, 10);
+
+/*accept() will return a brand new socket file descriptor	
+ *the original one is still listening on the port while the new one is ready to send() and recv()
+ *int accept(int sockfd, void *addr, int *addrlen);
+ *addr will be a pointer to a local struct sockaddr_in where the incoming connection will go
+ *addrlen should be set to sizeof(struct sockaddr_in) before its address is passed to accept()
+*/
+	int new_fd = accept(my_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
+// new_fd will be used for all send() and recv() calls
+
+/*
+ *	int send(int sockfd, const void *msg, int len, int flags);
+ *Just set flags to 0
+ *send() returns the number of bytes actually sent out
+ *sockfd is the socket descriptor you want to send data to
+*/	
+	char *msg = "hi";
+	int len, bytes_sent;
+	len = strlen(msg);
+	bytes_sent = send(new_fd, msg, len, 0);
+
+/*
+ *	int recv(int sockfd, void *buf, int len, unsigned int flags);
+ *sockfd is the socket descriptor to read from
+ *buf is the buffer to read information into
+ *len is the maximum length of the buffer
+ *flags can be set to 0
+ *If recv() returns 0, it means the remote side has closed the connection
+*/
+
+	close(my_sock);
 
 	return 0;
 }
